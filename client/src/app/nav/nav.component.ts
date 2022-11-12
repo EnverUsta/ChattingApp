907f6852a1cbbde627346a167cfbc14e4465beb8
EnverUsta <enveruusta@gmail.com>
2022-11-13 01:58:42 +0300

    add template driven form in angular

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { AppUser } from 'src/app/models/user.interface';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-  constructor() { }
+  user: AppUser = {
+    username: '',
+    password: '',
+  };
+
+  constructor() {}
+
+  login() {
+    console.log('user: ', this.user);
+  }
 }
