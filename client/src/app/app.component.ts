e5c7314f3d6bea766e6386a16020c971efdf2d50
EnverUsta <enveruusta@gmail.com>
2022-11-13 23:27:51 +0300

    feat(routing): create new components and their routings

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { UserDto } from './models/User/userDto.interface';
 import { AccountService } from './services/account.service';
