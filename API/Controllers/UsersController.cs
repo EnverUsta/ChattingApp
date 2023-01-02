5828536f8a474faa1cf8c3dc26bccfbd5ea0bb0a
EnverUsta <enveruusta@gmail.com>
2023-01-03 02:45:47 +0300

    feat(UsersControlerr): adds AddPhoto method

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using API.DTOs;
+using API.Entities;
 using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
@@ -53,9 +54,29 @@ public class UsersController : BaseApiController
         return BadRequest("Failed to update user");
     }
 
-    // [HttpPost("add-photo")]
-    // public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
-    // {
-    //     var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
-    // }
+    [HttpPost("add-photo")]
+    public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+    {
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+        if (user == null) return NotFound();
+
+        var result = await _photoService.AddPhotoAsync(file);
+
+        if (result.Error != null) return BadRequest(result.Error.Message);
+
+        var photo = new Photo
+        {
+            Url = result.SecureUrl.AbsoluteUri,
+            PublicId = result.PublicId
+        };
+
+        if (user.Photos.Count == 0) photo.IsMain = true;
+
+        user.Photos.Add(photo);
+
+        if (await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDto>(photo);
+
+        return BadRequest("Problem adding photo");
+    }
 }
