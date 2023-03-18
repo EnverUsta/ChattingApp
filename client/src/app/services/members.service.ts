85099bc4dc6ade42d851ceab43c3cfe02515e9cd
EnverUsta <enveruusta@gmail.com>
2023-03-18 22:53:06 +0300

    feat: like function in the client

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -110,6 +110,14 @@ export class MembersService {
     );
   }
 
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string) {
+    return this.http.get(this.baseUrl + 'likes?predicate=' + predicate);
+  }
+
   private getPaginatedResult<T>(url: string, params: HttpParams) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
     return this.http
