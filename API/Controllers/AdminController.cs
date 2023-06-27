205cdfb62b6d966347c53f707311c51a6bf932a4
EnverUsta <enver.usta@piworks.net>
2023-06-27 17:56:12 +0300

    feat(AdminController.cs): getting the users with roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,15 +1,34 @@
+using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AdminController : BaseApiController
 {
+    private readonly UserManager<AppUser> _userManager;
+
+    public AdminController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
     [Authorize(policy: "RequireAdminRole")]
     [HttpGet("users-with-roles")]
-    public ActionResult GetUsersWithRoles()
+    public async Task<ActionResult> GetUsersWithRoles()
     {
-        return Ok("Only admins can see this");
+        var users = await _userManager.Users
+            .OrderBy(user => user.UserName)
+            .Select(user => new
+            {
+                user.Id,
+                Username = user.UserName,
+                Roles = user.UserRoles.Select(r => r.Role.Name).ToList()
+            }).ToListAsync();
+
+        return Ok(users);
     }
 
     [Authorize(policy: "ModeratePhotoRole")]
