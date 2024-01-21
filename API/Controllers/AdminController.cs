245bf770b0a1315e0d56737963626bc5a3f65e9f
EnverUsta <enveruusta@gmail.com>
2024-01-21 06:26:38 +0300

    feat (AdminController.cs): Add edit roles ability to admin

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -29,6 +30,31 @@ public class AdminController : BaseApiController
             }).ToListAsync();
 
         return Ok(users);
+    }
+
+    [Authorize(policy: "RequireAdminRole")]
+    [HttpPost("edit-roles/{username}")]
+    public async Task<ActionResult> EditRoles(string username, [FromQuery] string roles)
+    {
+        if (string.IsNullOrEmpty(roles)) return BadRequest("You must select at least one role");
+
+        var selectedRoles = roles.Split(",").ToArray();
+
+        var user = await _userManager.FindByNameAsync(username);
+
+        if (user == null) return NotFound();
+
+        var userRoles = await _userManager.GetRolesAsync(user);
+
+        var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+        if (!result.Succeeded) return BadRequest("Failed to add to roles");
+
+        result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+        if (!result.Succeeded) return BadRequest("Failed to remove from roles");
+
+        return Ok(await _userManager.GetRolesAsync(user));
     }
 
     [Authorize(policy: "ModeratePhotoRole")]
